bb7131e2d64bd0e891d3bf7588db426ac90854fa
z1ch <tv.z1ch.tv@gmail.com>
2019-08-20 21:08:04 +0700

    Не смог доделать :( Не считаеться количество открытий Так же не понял строчку <a href="showIdPic.php?id='.$row["id"].'&db='.stphp.galary.'" (Да, списывал, иначе вообще бы не понял) И еще, почему у меня стили с отдельного файла не работают?)

---
+++ №5/config/connectSQL.php
@@ -0,0 +1,10 @@
+<?php
+
+define ("SQL_SERVER", "localhost");
+define ("SQL_USER", "root");
+define ("SQL_PASS", "");
+define ("SQL_BASE", "stphp");
+
+$connect = mysqli_connect(SQL_SERVER, SQL_USER, SQL_PASS, SQL_BASE);
+
+?>
--- №5/config/index.php
+++ №5/config/index.php
@@ -1,45 +1,26 @@
-<?php
-
-
-$link = mysqli_connect("localhost", "root", "", "stphp");
-
-$query = "SELECT * FROM stphp.galary;";
-
-$result = mysqli_query ($link, $query);
-
-$employess = [];
-
-while($row = mysqli_fetch_assoc($result)){ 
-   $epms[] = $row;
-};
-
-print_r ($employess);
-
-mysqli_close($link);
-
-?>
-
-
-
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../public/css/Style.css">
-    <title>#4</title>
+    <title>#5</title>
 </head>
 <body>
 
-<a href="../public/img/1.jpg" class="link"><img src="../public/img/1.jpg" alt=""></a>
-<a href="../public/img/2.jpg" class="link"><img src="../public/img/2.jpg" alt=""></a>
-<a href="../public/img/3.jpg" class="link"><img src="../public/img/3.jpg" alt=""></a>
-<a href="../public/img/4.jpg" class="link"><img src="../public/img/4.jpg" alt=""></a>
-<a href="../public/img/5.jpg" class="link"><img src="../public/img/5.jpg" alt=""></a>
-<a href="../public/img/6.jpg" class="link"><img src="../public/img/6.jpg" alt=""></a>
-<a href="../public/img/7.jpg" class="link"><img src="../public/img/7.jpg" alt=""></a>
-<a href="../public/img/8.jpg" class="link"><img src="../public/img/8.jpg" alt=""></a>
+    <div class="main">
+        <div class="galary">
+            <?php include 'showImg.php' ?>
+        </div>
+    </div>
+    <div class="active">
+        <form action="showIdPic.php" enctype="multipart/form-data" method="GET">
+            <h3>Выбрать id фото</h3>
+            <input type="number" name="id" value="1">
+            <input type="submit" value="Открыть">   
+        </form>        
+    </div>
 
 
 
 </body>
-</html> -->
+</html>
